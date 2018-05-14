93766f1233599f6903348a1a73de66342dd2e7ea
maerrco <31056101+maerrco@users.noreply.github.com>
2018-05-14 22:23:40 +1200

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,15 @@ restService.post("/sdpFailed", function(req, res) {
         break;
     }
   }
+  else if(req.body.result.action == "failedSE") {
+    switch (req.body.result.parameters.chosenMajor.toLowerCase()) {
+      //Speech Synthesis Markup Language 
+      case "software development":
+        speech =
+          'COMP602 (Software Development Practice) is needed for second year of the Software Development major. Since you failed it, im afraid you will have to repeat it if you want to continue with this major.';
+        break;
+    }
+  }
   else {}
   return res.json({
     speech: speech,
