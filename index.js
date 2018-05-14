4c4c59d43c6c647a5051c80490e5071371dc1d4c
maerrco <31056101+maerrco@users.noreply.github.com>
2018-05-14 22:20:16 +1200

    Update index.js

--- index.js
+++ index.js
@@ -29,13 +29,16 @@ restService.post("/echo", function(req, res) {
 
 restService.post("/sdpFailed", function(req, res) {
   var speech = "";
-  switch (req.body.result.parameters.chosenMajor.toLowerCase()) {
-    //Speech Synthesis Markup Language 
-    case "software development":
-      speech =
-        'COMP602 (Software Development Practice) is needed for second year of the Software Development major. Since you failed it, im afraid you will have to repeat it if you want to continue with this major.';
-      break;
+  if(req.body.result.action == "failedSDP") {
+    switch (req.body.result.parameters.chosenMajor.toLowerCase()) {
+      //Speech Synthesis Markup Language 
+      case "software development":
+        speech =
+          'COMP602 (Software Development Practice) is needed for second year of the Software Development major. Since you failed it, im afraid you will have to repeat it if you want to continue with this major.';
+        break;
+    }
   }
+  else {}
   return res.json({
     speech: speech,
     displayText: speech,
