e188530e6736ccb0c4f4754b125956808bfbb9e3
maerrco <31056101+maerrco@users.noreply.github.com>
2018-05-15 20:02:44 +1200

    Update index.js

--- index.js
+++ index.js
@@ -28,25 +28,9 @@ restService.post("/echo", function(req, res) {
 });
 
 restService.post("/sdpFailed", function(req, res) {
-   var papers = [{ paperName: "comp602",
-                    preReq: "comp603, comp610",
-                    coreq: "comp600",
-                    sd: "-",
-                    ns: "none",
-                    iss: "none",
-                    cs: "none",
-                    ci: "none",
-                    als: "none" },
-                    { paperName: "ense701",
-                    preReq: "comp603, comp610",
-                    coreq: "comp600",
-                    sd: "-",
-                    ns: "none",
-                    iss: "none",
-                    cs: "none",
-                    ci: "none",
-                    als: "none" }
-                    ]
+   var papers = [{ paperName: "comp602", preReq: "comp603, comp610", coreq: "comp600", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" },
+                 { paperName: "ense701", preReq: "comp603, comp610", coreq: "There are no co-requistes", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" }
+                ]
   
   var speech = "";
   var chosenPaper = papers.find(function (obj) { return obj.paperName === req.body.result.parameters.paper; });
@@ -55,7 +39,6 @@ restService.post("/sdpFailed", function(req, res) {
   if(majorVariable == "-") {
         speech = 'unfortunately, ' + chosenPaper.paperName + 'is a compulsary paper in your major. If you want to graduate in this major you will have to repeat '+ chosenPaper.paperName; 
   }
-  
   else {
         speech = 'Sorry, I only know about papers in the Computer Science Course :('
   }
