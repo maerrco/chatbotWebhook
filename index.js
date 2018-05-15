26826924e54a4ef6cf4d8db5c5835b0ecdd30c1e
maerrco <31056101+maerrco@users.noreply.github.com>
2018-05-15 15:59:50 +1200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const restService = express();
+
 restService.use(
   bodyParser.urlencoded({
     extended: true
@@ -26,44 +28,17 @@ restService.post("/echo", function(req, res) {
 });
 
 restService.post("/sdpFailed", function(req, res) {
-//     var comp602 = { paperName: "comp602",
-//                     preReq: "comp603, comp610",
-//                     coreq: "comp600",
-//                     sd: "-",
-//                     ns: "none",
-//                     iss: "none",
-//                     cs: "none",
-//                     ci: "none",
-//                     als: "none" };
-//     var ense701 = { paperName: "ense701",
-//                     preReq: "comp603, comp610",
-//                     coreq: "comp600",
-//                     sd: "-",
-//                     ns: "none",
-//                     iss: "none",
-//                     cs: "none",
-//                     ci: "none",
-//                     als: "none" };
-
-     
-//     var papers = new Array();
-//     papers.push(comp602);
-//     papers.push(ense701);
-    
   var speech = "";
-  
-//   var chosenPaper = papers.filter(function( obj ) {
-//     return obj.paperName == req.body.result.parameters.paper;
-//   });
-  
-//   var chosenMajor = req.body.result.parameters.chosenMajor.toLowerCase();
-  
-//   if(chosenPaper.sd == "-") {
-//         speech = chosenPaper + ' is needed for second year of the ' + chosenMajor + 'major. Since you failed it, im afraid you will have to repeat it if you want to continue with this major.';
-//     }
-//   }
-                 
-  if(req.body.result.parameters.paper == "ense701") {
+  if(req.body.result.parameters.paper == "comp602") {
+    switch (req.body.result.parameters.chosenMajor.toLowerCase()) {
+      //Speech Synthesis Markup Language 
+      case "software development":
+        speech =
+          'COMP602 (Software Development Practice) is needed for second year of the Software Development major. Since you failed it, im afraid you will have to repeat it if you want to continue with this major.';
+        break;
+    }
+  }
+  else if(req.body.result.parameters.paper == "ense701") {
     switch (req.body.result.parameters.chosenMajor.toLowerCase()) {
       //Speech Synthesis Markup Language 
       case "software development":
