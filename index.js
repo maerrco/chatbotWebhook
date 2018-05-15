d12652685538672b6e9cefe21fcfc7df7164f420
maerrco <31056101+maerrco@users.noreply.github.com>
2018-05-16 11:32:52 +1200

    Update index.js

--- index.js
+++ index.js
@@ -24,19 +24,19 @@ restService.post("/sdpFailed", function(req, res) {
                  { paperName: "comp603", preReq: "Either Pre Requisite: comp503 or comp610 or ense501", year: "Year 2", coreq: "no co-requisites", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" },
                  { paperName: "comp604", preReq: "Pre Requisite: comp502 and either one of comp503 or ense502", year: "Year 2", coreq: "no co-requisites", sd: "+", ns: "none", iss: "none", cs: "none", ci: "none", als: "none", options: "Infs602" },
                  { paperName: "comp704", preReq: "no pre-requisites", year: "Year 3", coreq: "no co-requisites", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" },
-                 { paperName: "comp713", preReq: "Pre Requisite: comp611", year: "Year 3", coreq: "no co-requisites", sd: "+", ns: "none", iss: "none", cs: "none", ci: "none", als: "none", options: "comp721" },
+                 { paperName: "comp713", preReq: "Pre Requisite: comp611", year: "Year 3", coreq: "no co-requisites", sd: "+", ns: "none", iss: "none", cs: "none", ci: "none", als: "none", options: "Comp721" },
                  { paperName: "comp719", preReq: "no pre-requisites", year: "Year 3", coreq: "no co-requisites", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" },
-                 { paperName: "comp721", preReq: "Pre Requisite: comp603", year: "Year 3", coreq: "no co-requisites", sd: "+", ns: "none", iss: "none", cs: "none", ci: "none", als: "none", options: "comp713" },
+                 { paperName: "comp721", preReq: "Pre Requisite: comp603", year: "Year 3", coreq: "no co-requisites", sd: "+", ns: "none", iss: "none", cs: "none", ci: "none", als: "none", options: "Comp713" },
                  { paperName: "enel504", preReq: "no pre-requisites", year: "Year 1", coreq: "no co-requistes", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" },
                  { paperName: "ense701", preReq: "Either Pre Requisite: comp603 or comp610", year: "Year 3", coreq: "no co-requistes", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" },
                  { paperName: "infs500", preReq: "no pre-requisites", year: "Year 1", coreq: "no co-requistes", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" },
                  { paperName: "infs600", preReq: "Either Pre Requisite: comp500 or ense501", year: "Year 2", coreq: "no co-requisites", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" },
                  { paperName: "infs601", preReq: "Either Pre Requisite: comp500 or ense501", year: "Year 2", coreq: "no co-requisites", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" },
                  { paperName: "infs602", preReq: "Pre Requisite: infs601 and either one of comp503 or ense502", year: "Year 2", coreq: "no co-requisites", sd: "+", ns: "none", iss: "none", cs: "none", ci: "none", als: "none", options: "Comp604" },
-                 { paperName: "math500", preReq: "no pre-requisites", year: "Year 1", coreq: "no co-requistes", sd: "+", ns: "none", iss: "none", cs: "none", ci: "none", als: "none", options: "Math501, Math502, Stat500" },
-                 { paperName: "math501", preReq: "no pre-requisites", year: "Year 1", coreq: "no co-requistes", sd: "+", ns: "none", iss: "none", cs: "none", ci: "none", als: "none", options: "Math500, Math502, Stat500" },
-                 { paperName: "math502", preReq: "no pre-requisites", year: "Year 1", coreq: "no co-requistes", sd: "+", ns: "none", iss: "none", cs: "none", ci: "none", als: "none", options: "Math500, Math501, Stat500" },
-                 { paperName: "stat500", preReq: "no pre-requisites", year: "Year 1", coreq: "no co-requistes", sd: "+", ns: "none", iss: "none", cs: "none", ci: "none", als: "none", options: "Math500, Math501, Math502" }
+                 { paperName: "math500", preReq: "no pre-requisites", year: "Year 1", coreq: "no co-requistes", sd: "+", ns: "none", iss: "none", cs: "none", ci: "none", als: "none", options: "Math501 or Math502 or Stat500" },
+                 { paperName: "math501", preReq: "no pre-requisites", year: "Year 1", coreq: "no co-requistes", sd: "+", ns: "none", iss: "none", cs: "none", ci: "none", als: "none", options: "Math500 or Math502 or Stat500" },
+                 { paperName: "math502", preReq: "no pre-requisites", year: "Year 1", coreq: "no co-requistes", sd: "+", ns: "none", iss: "none", cs: "none", ci: "none", als: "none", options: "Math500 or Math501 or Stat500" },
+                 { paperName: "stat500", preReq: "no pre-requisites", year: "Year 1", coreq: "no co-requistes", sd: "+", ns: "none", iss: "none", cs: "none", ci: "none", als: "none", options: "Math500 or Math501 or Math502" }
                 ]
    
    var chosenPaper = papers.find(function (obj) { return obj.paperName === req.body.result.parameters.paper; });
@@ -53,7 +53,7 @@ restService.post("/sdpFailed", function(req, res) {
       speech = 'This paper is not part of your major, you shouldnt have to be doing ' + chosenPaper.paperName;
     }
     else if(majorVariable == "+") {
-      speech = chosenPaper.paperName + ' is not compulsary for your major. you can still take either of these ' + chosenPaper.year + ' papers if you want to continue with your major: ' + chosenPaper.options;
+      speech = chosenPaper.paperName + ' is not compulsary for your major. you can still take a different ' + chosenPaper.year + ' paper if you want to continue with your major, such as: ' + chosenPaper.options;
     }      
     else {
       speech = 'Sorry, I only know about papers in the Computer Science Course :('
