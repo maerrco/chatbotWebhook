96638248c926cfda72eb9c37a4be67f36eaba34b
maerrco <31056101+maerrco@users.noreply.github.com>
2018-05-15 20:40:31 +1200

    Update index.js

--- index.js
+++ index.js
@@ -14,20 +14,38 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/sdpFailed", function(req, res) {
-   var papers = [{ paperName: "comp602", preReq: "comp603, comp610", coreq: "comp600", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" },
-                 { paperName: "ense701", preReq: "comp603, comp610", coreq: "There are no co-requistes", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" }
+   var papers = [{ paperName: "comp602", preReq: "Pre Requisites: comp603, comp610", coreq: "Co Requisites: comp600", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" },
+                 { paperName: "ense701", preReq: "Pre Requisites: comp603, comp610", coreq: "There are no co-requistes", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" }
                 ]
   
   var speech = "";
-  var chosenPaper = papers.find(function (obj) { return obj.paperName === req.body.result.parameters.paper; });
-  var majorVariable = chosenPaper[req.body.result.parameters.chosenMajor.toLowerCase()];
   
   if(req.body.result.action == "failedPaper") { 
+    var chosenPaper = papers.find(function (obj) { return obj.paperName === req.body.result.parameters.paper; });
+    var majorVariable = chosenPaper[req.body.result.parameters.chosenMajor.toLowerCase()];
+    
     if(majorVariable == "-") {
-          speech = 'unfortunately, ' + chosenPaper.paperName + ' is a compulsary paper in your major. If you want to graduate in this major you will have to repeat '+ chosenPaper.paperName; 
+      speech = 'Unfortunately, ' + chosenPaper.paperName + ' is a compulsary paper in your major. If you want to graduate in this major you will have to repeat '+ chosenPaper.paperName; 
+    }
+    else if(majorVariable == "none") {
+      speech = 'This paper is not part of your major, you shouldnt have to be doing ' + chosenPaper.paperName;
     }
     else {
-          speech = 'Sorry, I only know about papers in the Computer Science Course :('
+      speech = 'Sorry, I only know about papers in the Computer Science Course :('
+    }
+  }
+  
+  else if(req.body.result.action == "requestRequisites") {
+    var chosenPaper = papers.find(function (obj) { return obj.paperName === req.body.result.parameters.paper; });
+    
+    if(typeof req.body.result.paramaters.requisites1 != 'undefined' && typeof req.body.result.paramaters.requisites2 != 'undefined') {
+      speech = 'chosenPaper.paperName has, ' + chosenPaper.preReq + " and " +  chosenPaper.coReq;
+    }
+    else if(typeof req.body.result.paramaters.requisites1 == 'undefined' && typeof req.body.result.paramaters.requisites2 != 'undefined') {
+      speech = 'chosenPaper.paperName has, ' + chosenPaper.coReq;
+    }
+    else if(typeof req.body.result.paramaters.requisites1 != 'undefined' && typeof req.body.result.paramaters.requisites2 == 'undefined') {
+      speech = 'chosenPaper.paperName has, ' + chosenPaper.preReq;
     }
   }
   
