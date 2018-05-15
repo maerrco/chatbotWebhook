10fcf58d39ba079db51985430a38c951afd7532b
maerrco <31056101+maerrco@users.noreply.github.com>
2018-05-15 20:03:47 +1200

    Update index.js

--- index.js
+++ index.js
@@ -13,20 +13,6 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
-});
-
 restService.post("/sdpFailed", function(req, res) {
    var papers = [{ paperName: "comp602", preReq: "comp603, comp610", coreq: "comp600", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" },
                  { paperName: "ense701", preReq: "comp603, comp610", coreq: "There are no co-requistes", sd: "-", ns: "none", iss: "none", cs: "none", ci: "none", als: "none" }
@@ -37,7 +23,7 @@ restService.post("/sdpFailed", function(req, res) {
   var majorVariable = chosenPaper[req.body.result.parameters.chosenMajor.toLowerCase()];
   
   if(majorVariable == "-") {
-        speech = 'unfortunately, ' + chosenPaper.paperName + 'is a compulsary paper in your major. If you want to graduate in this major you will have to repeat '+ chosenPaper.paperName; 
+        speech = 'unfortunately, ' + chosenPaper.paperName + ' is a compulsary paper in your major. If you want to graduate in this major you will have to repeat '+ chosenPaper.paperName; 
   }
   else {
         speech = 'Sorry, I only know about papers in the Computer Science Course :('
@@ -50,80 +36,6 @@ restService.post("/sdpFailed", function(req, res) {
   });
 });
 
-restService.post("/video", function(req, res) {
-  return res.json({
-    speech:
-      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-    displayText:
-      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-    source: "webhook-echo-sample"
-  });
-});
-
-restService.post("/slack-test", function(req, res) {
-  var slack_message = {
-    text: "Details of JIRA board for Browse and Commerce",
-    attachments: [
-      {
-        title: "JIRA Board",
-        title_link: "http://www.google.com",
-        color: "#36a64f",
-
-        fields: [
-          {
-            title: "Epic Count",
-            value: "50",
-            short: "false"
-          },
-          {
-            title: "Story Count",
-            value: "40",
-            short: "false"
-          }
-        ],
-
-        thumb_url:
-          "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
-      },
-      {
-        title: "Story status count",
-        title_link: "http://www.google.com",
-        color: "#f49e42",
-
-        fields: [
-          {
-            title: "Not started",
-            value: "50",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          }
-        ]
-      }
-    ]
-  };
-  return res.json({
-    speech: "speech",
-    displayText: "speech",
-    source: "webhook-echo-sample",
-    data: {
-      slack: slack_message
-    }
-  });
-});
 
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
