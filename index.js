1e50d1243a71b39ac6cd55e3ec7923a9095fd38a
maerrco <31056101+maerrco@users.noreply.github.com>
2018-05-15 14:52:17 +1200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const restService = express();
 
 restService.use(
@@ -12,40 +13,22 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-    var comp602 = { paperName: "comp602",
-                    preReq: "comp603, comp610",
-                    coreq: "comp600",
-                    softwareDevelopment: "-",
-                    networking: "none",
-                    iss: "none",
-                    computerScience: "none",
-                    ci: "none",
-                    analytics: "none" };
-    var ense701 = { paperName: "ense701",
-                    preReq: "comp603, comp610",
-                    coreq: "comp600",
-                    softwareDevelopment: "-",
-                    networking: "none",
-                    iss: "none",
-                    computerScience: "none",
-                    ci: "none",
-                    analytics: "none" };
-
-     
-var papers = new Array();
-papers.push(comp602);
-papers.push(ense701);
-
+restService.post("/echo", function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.echoText
+      ? req.body.result.parameters.echoText
+      : "Seems like some problem. Speak again.";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+});
 
 restService.post("/sdpFailed", function(req, res) {
-  //store papers with their corresponding papers
   var speech = "";
-  
-  var chosenPaper = papers.filter(function( obj ) {
-    return obj. == 6;
-  });
-  
-  
   if(req.body.result.parameters.paper == "comp602") {
     switch (req.body.result.parameters.chosenMajor.toLowerCase()) {
       //Speech Synthesis Markup Language 
@@ -60,16 +43,8 @@ restService.post("/sdpFailed", function(req, res) {
       //Speech Synthesis Markup Language 
       case "software development":
         speech =
-          'Unfortunately, ENSE701 (Contemporary Methods in Software Engineering) is needed for third year of the Software Development major. You will have to repeat it if you want to continue with the major';
+          'You have 2 options:';
         break;
-    }
-    else if(req.body.result.parameters.paper == "ense701") {
-      switch (req.body.result.parameters.chosenMajor.toLowerCase()) {
-        //Speech Synthesis Markup Language 
-        case "software development":
-          speech =
-            'Unfortunately, ENSE701 (Contemporary Methods in Software Engineering) is needed for third year of the Software Development major. You will have to repeat it if you want to continue with the major';
-          break;
     }
   }
   else {}
@@ -77,6 +52,81 @@ restService.post("/sdpFailed", function(req, res) {
     speech: speech,
     displayText: speech,
     source: "webhook-echo-sample"
+  });
+});
+
+restService.post("/video", function(req, res) {
+  return res.json({
+    speech:
+      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    displayText:
+      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    source: "webhook-echo-sample"
+  });
+});
+
+restService.post("/slack-test", function(req, res) {
+  var slack_message = {
+    text: "Details of JIRA board for Browse and Commerce",
+    attachments: [
+      {
+        title: "JIRA Board",
+        title_link: "http://www.google.com",
+        color: "#36a64f",
+
+        fields: [
+          {
+            title: "Epic Count",
+            value: "50",
+            short: "false"
+          },
+          {
+            title: "Story Count",
+            value: "40",
+            short: "false"
+          }
+        ],
+
+        thumb_url:
+          "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
+      },
+      {
+        title: "Story status count",
+        title_link: "http://www.google.com",
+        color: "#f49e42",
+
+        fields: [
+          {
+            title: "Not started",
+            value: "50",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          }
+        ]
+      }
+    ]
+  };
+  return res.json({
+    speech: "speech",
+    displayText: "speech",
+    source: "webhook-echo-sample",
+    data: {
+      slack: slack_message
+    }
   });
 });
 
